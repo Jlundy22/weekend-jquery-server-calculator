63a13397cbc2429e0cfc1f2d3eb59fd34a0e1859
Jack Lund <jacklund920@gmail.com>
2022-04-15 19:22:42 -0500

    all buttons hookded up via jquery

--- server/public/client.js
+++ server/public/client.js
@@ -1,6 +1,46 @@
-console.log('Jello');
+let operator;
+
+
 $(document).ready(onReady);
 
 function onReady() {
-    console.log('JaCrispy')
+    $('#plusButton').on('click', plusButtonClick );
+    $('#minusButton').on('click', minusButtonClick );
+    $('#multiplicationButton').on('click', multiplicationButtonClick);
+    $('#divideButton').on('click', divideButtonClick );
+    $('#equalButton').on('click', equalsButtonClick );
+    $('#clearButton').on('click', clearButtonClick );
+
+    
+}
+
+function plusButtonClick() {
+    operator = '+';
+    console.log(operator);
+
+}
+function minusButtonClick() {
+    operator = '-';
+    console.log(operator)
+
+    
+}
+function multiplicationButtonClick() {
+    operator = '*';
+    console.log(operator);
+    
+}
+function divideButtonClick() {
+    operator = '/';
+    console.log(operator);
+    
+}
+function equalsButtonClick() {
+
+    console.log('equals button clicked');
+    
+}
+function clearButtonClick() {
+    console.log('clear button clicked');
+    
 }
