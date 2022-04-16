ae307214aa22e02f31ae14f9de286a2842efc867
Jack Lund <jacklund920@gmail.com>
2022-04-15 19:59:10 -0500

    initial server setup complete

---
+++ server/server.js
@@ -0,0 +1,25 @@
+
+////////////////////////////////////////////////////////////
+// Server initialization
+////////////////////////////////////////////////////////////
+const express = require('express');
+const bodyParser = require('body-parser');
+const res = require('express/lib/response');
+const { send } = require('express/lib/response');
+const app = express();
+const PORT = 5000;
+
+// This must be added before GET & POST routes.
+app.use(bodyParser.urlencoded({extended:true}))
+
+// Serve up static files (HTML, CSS, Client JS)
+app.use(express.static('server/public'));
+
+// GET & POST Routes go here
+
+
+app.listen(PORT, () => {
+  console.log ('Server is running on port', PORT)
+})
+
+
